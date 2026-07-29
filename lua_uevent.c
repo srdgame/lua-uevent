76f10918bd2b9b20f8250202ed86ffb938c773d1
Dirk Chang <dirk@kooiot.com>
2026-07-29 19:10:18 +0800

    using timeout to fix recvmsg block issue

--- lua_uevent.c
+++ lua_uevent.c
@@ -30,14 +30,13 @@ typedef struct {
 } conn_callback_t;
 
 typedef struct {
+	lua_State* state;
 	conn_callback_t * callback;
 	pthread_t thread;
 	pthread_mutex_t lock;
-	pthread_mutex_t thread_lock;  // Lock for thread startup/shutdown
 	int sock;
+	int closing;
 	int closed;
-	int running;  // Flag to indicate thread is running
-	int in_callback;  // Flag to prevent recursive calls
 	int env;
 	struct data_list_node *data_head;
 	struct data_list_node *data_tail;
@@ -105,10 +104,7 @@ static int traceback (lua_State *L) {
 static void uevent_check_callback(lua_State* L, conn_callback_t *cb, int index)
 {
 	luaL_checktype(L, index, LUA_TFUNCTION);
-	// Only unref if it's a valid reference (not LUA_NOREF)
-	if (cb->callback != LUA_NOREF) {
-		luaL_unref(L, LUA_REGISTRYINDEX, cb->callback);
-	}
+	luaL_unref(L, LUA_REGISTRYINDEX, cb->callback);
 	lua_pushvalue(L, index);
 	cb->callback = luaL_ref(L, LUA_REGISTRYINDEX);
 }
@@ -118,35 +114,29 @@ static void uevent_call_callback(lua_State* L, conn_callback_t* cb, int nargs) {
 	if (ref == LUA_NOREF) {
 		lua_pop(L, nargs);
 	} else {
-		int traceback_idx = 0;
-		int base = lua_gettop(L) - nargs + 1;
-
-		// Get the traceback function
+		// Get the traceback function in case of error
 		lua_pushcfunction(L, traceback);
-		traceback_idx = lua_gettop(L);
+
+		// And insert it before the args if there are any.
+		if (nargs) {
+			lua_insert(L, -1 - nargs);
+		}
 
 		// Get the callback
 		lua_rawgeti(L, LUA_REGISTRYINDEX, ref);
 
-		// Move callback and traceback before args
-		if (nargs > 0) {
-			for (int i = 0; i < nargs; i++) {
-				lua_insert(L, base);
-			}
-			// Now stack is: traceback, callback, args...
-			lua_insert(L, base);  // Move callback before traceback
-			// Now stack is: callback, traceback, args...
+		// And insert it before the args if there are any.
+		if (nargs) {
+			lua_insert(L, -1 - nargs);
 		}
 
-		if (lua_pcall(L, nargs, 0, traceback_idx)) {
+		// output error
+		if (lua_pcall(L, nargs, 0, -2 - nargs)) {
 			fprintf(stderr, "Uncaught Error: %s\n", lua_tostring(L, -1));
-			lua_pop(L, 1);  // Remove error message
 		}
 
-		// Remove the traceback function if still on stack
-		if (lua_gettop(L) >= traceback_idx) {
-			lua_pop(L, 1);
-		}
+		// Remove the traceback function
+		lua_pop(L, 1);
 	}
 }
 
@@ -159,78 +149,32 @@ static uevent_conn_t *get_connection (lua_State *L) {
 	return conn;
 }
 
+static int close_connection(uevent_conn_t* conn) {
+	if (conn != NULL && !conn->closed) {
+		// Mark connection as closed
+		conn->closing = 1;
+		pthread_join(conn->thread, NULL);
+	}
+	return 0;
+}
+
 static int uevent_gc(lua_State *L)
 {
 	uevent_conn_t* conn = get_connection(L);
-
-	if (conn != NULL && !(conn->closed)) {
-		// Mark connection as closed
-		conn->closed = 1;
-		if (conn->env != LUA_NOREF) {
-			luaL_unref (L, LUA_REGISTRYINDEX, conn->env);
-			conn->env = LUA_NOREF;
-		}
-
-		// Signal thread to stop
-		pthread_mutex_lock(&conn->thread_lock);
-		conn->running = 0;
-		pthread_mutex_unlock(&conn->thread_lock);
-
-		if (conn->sock >= 0) {
-			shutdown(conn->sock, SHUT_RDWR);
-			close(conn->sock);
-			conn->sock = -1;
-		}
-
-		// Wait for thread to exit before cleaning up
-		pthread_join(conn->thread, NULL);
-
-		// Now safe to clean up data list
-		pthread_mutex_lock(&conn->lock);
-
-		while (conn->data_head) {
-			data_list_node_t* node = conn->data_head;
-			conn->data_head = node->next;
-			free(node->data.data);
-			free(node);
-		}
-		conn->data_tail = NULL;
-
-		pthread_mutex_unlock(&conn->lock);
-
-		pthread_mutex_destroy(&conn->lock);
-
-		if (conn->callback) {
-			if (conn->callback->callback != LUA_NOREF) {
-				luaL_unref (L, LUA_REGISTRYINDEX, conn->callback->callback);
-			}
-			free(conn->callback);
-			conn->callback = NULL;
-		}
-
-		pthread_mutex_destroy(&conn->thread_lock);
-	}
+	close_connection(conn);
 	return 0;
 }
 
 static int uevent_close (lua_State *L) {
 	uevent_conn_t* conn = get_connection(L);
-	if (conn->closed) {
-		lua_pushboolean (L, 0);
-		return 1;
-	}
-	uevent_gc(L);
+	close_connection(conn);
 	lua_pushboolean (L, 1);
 	return 1;
 }
 
 static void process_data(lua_State* L, uevent_data_t* data)
 {
-	if (!data->conn || data->conn->closed) {
-		return;
-	}
-
-	if (!L) {
+	if (!L || !data->conn || data->conn->closing || data->conn->closed) {
 		return;
 	}
 
@@ -242,13 +186,15 @@ static void process_data(lua_State* L, uevent_data_t* data)
 static int uevent_run(lua_State *L)
 {
 	uevent_conn_t* conn = get_connection(L);
+	if (conn == NULL) {
+		return uevent_failmsg(L, "UEVENT mutex_lock error", strerror(errno));
+	}
+
 	if (conn->closed){
 		return uevent_failmsg(L, "UEVENT connection closed", "connection is closed");
 	}
-
-	// Prevent recursive calls
-	if (conn->in_callback) {
-		return uevent_failmsg(L, "UEVENT recursive call detected", "cannot call run() from within callback");
+	if (conn->closing){
+		return uevent_failmsg(L, "UEVENT connection closing", "connection is closing");
 	}
 
 	if (pthread_mutex_lock(&conn->lock) != 0) {
@@ -257,28 +203,24 @@ static int uevent_run(lua_State *L)
 
 	data_list_node_t* node = conn->data_head;
 	if (node != NULL) {
-		while (node) {
-			data_list_node_t* next = node->next;
+		while (node && !conn->closing && !conn->closed) {
+			conn->data_head = node->next;
 
-			// Mark that we're in callback
-			conn->in_callback = 1;
 			pthread_mutex_unlock(&conn->lock);
 
 			// Process data (callback is called here)
 			// Pass lua_State* directly to avoid race condition
 			process_data(L, &node->data);
-
-			pthread_mutex_lock(&conn->lock);
-			conn->in_callback = 0;
-
-			// Remove and free node
-			conn->data_head = next;
 			free(node->data.data);
 			free(node);
 
+			pthread_mutex_lock(&conn->lock);
+
+			// Get next node
 			node = conn->data_head;
 		}
-		conn->data_tail = conn->data_head;
+		// all node processed
+		conn->data_tail = NULL;
 	}
 
 	if (pthread_mutex_unlock(&conn->lock) != 0) {
@@ -321,6 +263,11 @@ static const struct luaL_Reg api_funcs[] = {
 
 static void push_conn_data(uevent_conn_t* conn, const char* buf, int len)
 {
+	// Check if connection is still valid
+	if (conn->closed || conn->closing) {
+		return;
+	}
+
 	data_list_node_t *node = (data_list_node_t*)malloc(sizeof(data_list_node_t));
 	if (!node) {
 		return;
@@ -347,14 +294,6 @@ static void push_conn_data(uevent_conn_t* conn, const char* buf, int len)
 		return;
 	}
 
-	// Check if connection is still valid
-	if (conn->closed) {
-		pthread_mutex_unlock(&conn->lock);
-		free(msg);
-		free(node);
-		return;
-	}
-
 	// Add to connection
 	if (conn->data_head == NULL) {
 		conn->data_head = node;
@@ -367,6 +306,7 @@ static void push_conn_data(uevent_conn_t* conn, const char* buf, int len)
 	pthread_mutex_unlock(&conn->lock);
 }
 
+static int destroy_connection (uevent_conn_t *conn);
 static void* connection_proc(void* arg)
 {
     uevent_conn_t *conn = (uevent_conn_t *)arg;
@@ -385,34 +325,18 @@ static void* connection_proc(void* arg)
     hdr.msg_iov = &iov;
     hdr.msg_iovlen = 1;
 
-    // Signal that thread has started
-    // Note: Lock errors here are acceptable as thread will check flags in loop
-    pthread_mutex_lock(&conn->thread_lock);
-    conn->running = 1;
-    pthread_mutex_unlock(&conn->thread_lock);
-
     while (1) {
-        // Check closed flag without lock (set only by main thread, safe to read)
-        if (conn->closed) {
-            break;
-        }
-
-        // Check running flag with lock (may be modified by other thread)
-        pthread_mutex_lock(&conn->thread_lock);
-        if (!conn->running) {
-            pthread_mutex_unlock(&conn->thread_lock);
-            break;
-        }
-        pthread_mutex_unlock(&conn->thread_lock);
-
-        // Check socket validity in each iteration
-        if (conn->sock < 0) {
+        if (conn->closing || conn->closed || conn->sock < 0) {
             break;
         }
 
         len = recvmsg(conn->sock, &hdr, 0);
 
-        if (len <= 0) {
+        if (len < 0) {
+			// timeout then continue;
+			if (errno == EAGAIN || errno == EWOULDBLOCK) {
+				continue;
+			}
             break;
         }
 
@@ -427,12 +351,8 @@ static void* connection_proc(void* arg)
 		push_conn_data(conn, msg, len);
 	}
 
-	// Signal thread is exiting
-	// Note: Lock errors here are acceptable as thread is terminating
-	pthread_mutex_lock(&conn->thread_lock);
-	conn->running = 0;
-	conn->sock = -1;
-	pthread_mutex_unlock(&conn->thread_lock);
+	// Socket closed or connection is closing.
+	destroy_connection(conn);
 
 	return NULL;
 }
@@ -463,6 +383,15 @@ static int create_netlink(lua_State *L, uevent_conn_t* conn, unsigned int groups
 		// return uevent_failmsg(L, "setsockopt error ", strerror(errno));
 	}
 
+	struct timeval tv;
+	tv.tv_sec = 1;   // 1秒超时
+	tv.tv_usec = 0;
+	ret = setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
+	if (ret < 0) {
+		close(sockfd);
+		return uevent_failmsg(L, "setsockopt RECVTIMEO error ", strerror(errno));
+	}
+
 	ret = bind(sockfd, (struct sockaddr *) &addr, sizeof(addr));
 	if (ret < 0) {
 		close(sockfd);
@@ -474,18 +403,8 @@ static int create_netlink(lua_State *L, uevent_conn_t* conn, unsigned int groups
 		return uevent_failmsg(L, "create lock failed ", strerror(errno));
 	}
 
-	if (pthread_mutex_init(&(conn->thread_lock), NULL) != 0) {
-		pthread_mutex_destroy(&conn->lock);
-		close(sockfd);
-		return uevent_failmsg(L, "create thread_lock failed ", strerror(errno));
-	}
-
-	conn->running = 0;
-	conn->in_callback = 0;
-
 	if (pthread_create(&conn->thread, NULL, connection_proc, conn) != 0) {
 		pthread_mutex_destroy(&conn->lock);
-		pthread_mutex_destroy(&conn->thread_lock);
 		close(sockfd);
 		return uevent_failmsg(L, "create thread failed ", strerror(errno));
 	}
@@ -497,6 +416,46 @@ static int create_netlink(lua_State *L, uevent_conn_t* conn, unsigned int groups
 	return 1;
 }
 
+static int destroy_connection(uevent_conn_t *conn) {
+	if (!conn || conn->closed) {
+		return 0;
+	}
+
+	// Now safe to clean up data list
+	pthread_mutex_lock(&conn->lock);
+
+	while (conn->data_head) {
+		data_list_node_t* node = conn->data_head;
+		conn->data_head = node->next;
+		free(node->data.data);
+		free(node);
+	}
+	conn->data_tail = NULL;
+
+	// Mark connection as closed
+	conn->closed = 1;
+	luaL_unref (conn->state, LUA_REGISTRYINDEX, conn->env);
+	conn->env = LUA_NOREF;
+
+	// Signal thread to stop
+	if (conn->sock >= 0) {
+		shutdown(conn->sock, SHUT_RDWR);  // Try to shutdown socket to wakup recvmsg
+		close(conn->sock);
+		conn->sock = -1;
+	}
+
+	if (conn->callback) {
+		luaL_unref (conn->state, LUA_REGISTRYINDEX, conn->callback->callback);
+		free(conn->callback);
+		conn->callback = NULL;
+	}
+	conn->state = NULL;
+
+	pthread_mutex_unlock(&conn->lock);
+	pthread_mutex_destroy(&conn->lock);
+	return 0;
+}
+
 static int create_connection (lua_State *L,  conn_callback_t *callback, int env, unsigned int groups) {
 	uevent_conn_t *conn = (uevent_conn_t *)lua_newuserdata(L, sizeof(uevent_conn_t));
 	luaL_getmetatable (L, UEVENT_HANDLE);
@@ -504,11 +463,11 @@ static int create_connection (lua_State *L,  conn_callback_t *callback, int env,
 
 	// Initialize structure
 	memset(conn, 0, sizeof(uevent_conn_t));
+	conn->state = L;
 	conn->callback = callback;
+	conn->closing = 0;
 	conn->closed = 0;
 	conn->sock = -1;
-	conn->running = 0;
-	conn->in_callback = 0;
 	conn->env = LUA_NOREF;
 	conn->data_head = NULL;
 	conn->data_tail = NULL;
